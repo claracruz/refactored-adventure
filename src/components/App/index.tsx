17aa8252c9747dfe9e15ac327279b4398b7b4ba4
claraudofa <claracruzz@yahoo.co.uk>
2019-02-18 18:41:58 +0000

    remove unneeded import

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React  from 'react';
 import { Grid } from '../Grid';
 import { SearchInput } from '../SearchInput';
 import { Loading } from '../Loading';
